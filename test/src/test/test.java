03d3439e7f4ace09018088be87e2bacee5039df8
Finn <Finn@DESKTOP-P48A284.home>
2022-03-02 17:30:13 +0100

    c

---
+++ test/src/test/test.java
@@ -0,0 +1,18 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		boolean sjekk = testing(4);
+		System.out.println(sjekk);
+
+	}
+	
+	public static boolean testing(int tall) {
+		if(tall == 5) {
+			return true;
+		}
+		return false;
+	}
+
+}
